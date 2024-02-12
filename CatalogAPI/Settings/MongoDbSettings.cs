d9aed46c9e582ebbe104f2a50abe97e775089b99
Erwin <erwin.paralesda@gmail.com>
2024-02-12 11:37:19 -0500

    Update MongoDbSettings.cs

--- CatalogAPI/Settings/MongoDbSettings.cs
+++ CatalogAPI/Settings/MongoDbSettings.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,14 +8,31 @@ namespace CatalogAPI.Settings
 {
     public class MongoDbSettings
     {
+
+        public MongoClient mongoClient;
+        public IMongoDatabase mongoDatabase;
+
+        public MongoDbSettings()
+        {
+            mongoClient = new MongoClient("mongodb://127.0.0.1:27017");
+
+            mongoDatabase = mongoClient.GetDatabase("Catalog");
+        }
+
+
         public string Host { get; set; }
         public string Port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
 
-        public string ConnectionString 
+
+
+        public string ConnectionString
         {
-            get 
+            get
             {
                 return $"mongodb://{Host}:{Port}";
+                //return $"mongodb://{User}:{Password}@{Host}:{Port}";
             }
         }
 
